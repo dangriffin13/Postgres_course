a5f86315495f45542ee793d68cab99efa6b2e9e5
dangriffin13 <dangriffin13@gmail.com>
2019-04-23 23:47:05 -0400

    de minimis

--- app_psycopg2/app.py
+++ app_psycopg2/app.py
@@ -1,9 +1,15 @@
 from user import User
+from database import Database
 
-my_user = User('ray@aol.com', 'Ray', 'Smith', None)
+Database.initialize(database='learning',
+                    user='danielgriffin',
+                    password='',
+                    host='localhost')
 
-my_user.save_to_db()
+my_user = User('jan@aol.com', 'Janet', 'Smith', None)
 
-user_from_db = User.load_from_db_by_email('ray@aol.com')
+#my_user.save_to_db()
+
+user_from_db = User.load_from_db_by_email('rolf@rsmith.com')
 
 print(user_from_db)
